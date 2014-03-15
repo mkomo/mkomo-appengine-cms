1e8b89853c80f44140080ca29779c9a9f943675c
Matt Komorowski <mkomorowski@synacor.com>
2014-03-14 23:48:31 -0400

    added djangoforms, as it is no longer supported

--- src/admin.py
+++ src/admin.py
@@ -5,7 +5,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
-from google.appengine.ext.db import djangoforms
+from lib import djangoforms
 
 try:
   from mkomo import Page, Asset, List, ModelAndView, MavRequestHandler
