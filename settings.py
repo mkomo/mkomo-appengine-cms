a7bd4b7a99b32bd7da9d074ea6e1334f3e75f87c
Matt Komorowski <matt@mkomo.com>
2014-04-04 21:23:08 -0400

    moved nav into content

--- settings.py
+++ settings.py
@@ -3,4 +3,5 @@ import os
 PROJECT_ROOT = os.path.dirname(__file__)
 TEMPLATE_DIRS = (PROJECT_ROOT,
                  os.path.join(PROJECT_ROOT, 'templates'),
-                 os.path.join(PROJECT_ROOT, 'content/pages'),)
+                 os.path.join(PROJECT_ROOT, 'content/pages'),
+                 os.path.join(PROJECT_ROOT, 'content/components'),)
